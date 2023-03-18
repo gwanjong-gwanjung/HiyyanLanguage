31cdd897fa97e40d1a9f108b3af2f29f53527b2c
gwanjong-gwanjung <boyboy0710@naver.com>
2023-03-18 17:34:46 +0900

    기본

--- src/main/kotlin/me/gwanjong/gwanjung/Main.kt
+++ src/main/kotlin/me/gwanjong/gwanjung/Main.kt
@@ -1,7 +1,95 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package me.gwanjong.gwanjung
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import java.io.File
+import java.io.IOException
+import java.util.*
+import kotlin.system.exitProcess
+
+val memory: MutableList<Int> = mutableListOf()
+
+var pc: Int = 0
+
+fun MutableList<Int>.getSafe(index: Int): Int {
+    return if(index > this.lastIndex) 0 else this[index]
+}
+
+fun MutableList<Int>.setSafe(index: Int, value: Int) {
+    if(index > this.lastIndex){
+        for(i in 0 until index - this.lastIndex){
+            this.add(0)
+        }
+    }
+    this[index] = value
+}
+
+fun parseLine(code: String){
+    if(code.endsWith("")){
+        if(code.replace("식?", "") == ""){
+            throw java.lang.RuntimeException("문법 오류: 대입할 변수 미지정 (${pc + 1}번 줄)")
+        }
+        else {
+            val sc = Scanner(System.`in`)
+            memory.setSafe(code.replace("식?", "").toHiyyanInt(), sc.nextInt())
+        }
+    }
+    else if(code.startsWith("식")){
+        if(code.endsWith("!")){
+            val q = code.replace("식", "").replace("!", "")
+            print("${q.toHiyyanInt()}")
+        }
+        else if(code.endsWith("ㅋ")){
+            val q = code.replace("식", "").replace("ㅋ", "")
+            if(q == ""){
+                println()
+            }
+            else {
+                val b = q.toHiyyanInt()
+
+                print(convertUnicodeToString("\\u${b.toString(16).padStart(4 - b.toString(16).length, '0')}"))
+            }
+        }
+    }
+
+    else if(code.contains("엄")){
+        val c = code.split("엄")
+
+        memory.setSafe(c[0].countChar("어"), c[1].toHiyyanInt())
+    }
+    else {
+        return
+    }
+    return
+}
+
+fun main(args: Array<String>) {
+    if(args.isEmpty()){
+        throw IOException("파일 이름이 없음")
+    }
+    else if(!args[0].endsWith(".hiyyanbabo")){
+        throw IOException("${args[0]} 작동")
+    }
+
+    val f = File(args[0])
+    if(!f.canRead()) {
+        throw IOException("파일을 읽을 수 없음")
+    }
+    else {
+        var codes = f.readText().trim().replace("~", "\n").split("\n")
+
+        if(codes[0] != "얀하" && codes.last() != "얀바"){
+            throw Exception("히바얀보")
+        }
+
+        codes = codes.subList(1, codes.size)
+
+        while(true){
+            if(codes[pc] != "얀바"){
+                parseLine(codes[pc].trim())
+                pc++
+            }
+            else break
+        }
+    }
+
+    exitProcess(0)
 }
